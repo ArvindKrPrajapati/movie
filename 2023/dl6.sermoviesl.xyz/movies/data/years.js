4b2a2942566905f890baf1d448205bbef7399582
ArvindKrPrajapati <arvindsoft37@gmail.com>
2023-04-27 18:46:02 +0530

    added one more server

---
+++ 2023/dl6.sermoviesl.xyz/movies/data/years.js
@@ -0,0 +1,115 @@
+const data = [
+{
+                       year:"1953", 
+                      href:"http://dl6.sermovie.xyz/Movie/1953/"
+                       },
+{
+                       year:"1960", 
+                      href:"http://dl6.sermovie.xyz/Movie/1960/"
+                       },
+{
+                       year:"1967", 
+                      href:"http://dl6.sermovie.xyz/Movie/1967/"
+                       },
+{
+                       year:"1980", 
+                      href:"http://dl6.sermovie.xyz/Movie/1980/"
+                       },
+{
+                       year:"1985", 
+                      href:"http://dl6.sermovie.xyz/Movie/1985/"
+                       },
+{
+                       year:"1986", 
+                      href:"http://dl6.sermovie.xyz/Movie/1986/"
+                       },
+{
+                       year:"1988", 
+                      href:"http://dl6.sermovie.xyz/Movie/1988/"
+                       },
+{
+                       year:"1989", 
+                      href:"http://dl6.sermovie.xyz/Movie/1989/"
+                       },
+{
+                       year:"1990", 
+                      href:"http://dl6.sermovie.xyz/Movie/1990/"
+                       },
+{
+                       year:"1992", 
+                      href:"http://dl6.sermovie.xyz/Movie/1992/"
+                       },
+{
+                       year:"1994", 
+                      href:"http://dl6.sermovie.xyz/Movie/1994/"
+                       },
+{
+                       year:"2003", 
+                      href:"http://dl6.sermovie.xyz/Movie/2003/"
+                       },
+{
+                       year:"2006", 
+                      href:"http://dl6.sermovie.xyz/Movie/2006/"
+                       },
+{
+                       year:"2008", 
+                      href:"http://dl6.sermovie.xyz/Movie/2008/"
+                       },
+{
+                       year:"2009", 
+                      href:"http://dl6.sermovie.xyz/Movie/2009/"
+                       },
+{
+                       year:"2010", 
+                      href:"http://dl6.sermovie.xyz/Movie/2010/"
+                       },
+{
+                       year:"2011", 
+                      href:"http://dl6.sermovie.xyz/Movie/2011/"
+                       },
+{
+                       year:"2012", 
+                      href:"http://dl6.sermovie.xyz/Movie/2012/"
+                       },
+{
+                       year:"2013", 
+                      href:"http://dl6.sermovie.xyz/Movie/2013/"
+                       },
+{
+                       year:"2014", 
+                      href:"http://dl6.sermovie.xyz/Movie/2014/"
+                       },
+{
+                       year:"2015", 
+                      href:"http://dl6.sermovie.xyz/Movie/2015/"
+                       },
+{
+                       year:"2016", 
+                      href:"http://dl6.sermovie.xyz/Movie/2016/"
+                       },
+{
+                       year:"2017", 
+                      href:"http://dl6.sermovie.xyz/Movie/2017/"
+                       },
+{
+                       year:"2018", 
+                      href:"http://dl6.sermovie.xyz/Movie/2018/"
+                       },
+{
+                       year:"2019", 
+                      href:"http://dl6.sermovie.xyz/Movie/2019/"
+                       },
+{
+                       year:"2021", 
+                      href:"http://dl6.sermovie.xyz/Movie/2021/"
+                       },
+{
+                       year:"2022", 
+                      href:"http://dl6.sermovie.xyz/Movie/2022/"
+                       },
+{
+                       year:"2023", 
+                      href:"http://dl6.sermovie.xyz/Movie/2023/"
+                       },
+]
+ module.exports={data}
---
+++ 2023/dl6.sermoviesl.xyz/movies/years.mjs
@@ -0,0 +1,197 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//find movie link from each folder
+
+
+//const destFile="./data/years.js"
+//const destFile="./data/movie_name.js"
+const destFile="./data/movie_link.js"
+const extraFile="./data/extra.js"
+import * as cheerio from 'cheerio';
+import * as fs from 'fs';
+//import {data} from "./data/years.js"
+import {data} from "./data/movie_name.js"
+const got = (...args) => import('got').then(({ default: got }) => got(...args));
+
+var count=0
+const extractLinks = async (obj) => {
+    try {
+        const response = await got(obj.href);
+        const html = response.body;
+        const $ = cheerio.load(html);
+        const linkObjects = $('a');
+        const sizeObj=$(".size")
+        
+        const links = [];
+         linkObjects.each((index, element) => {
+                if(!([0,1,2,3,4,5,6].includes(index))){
+                  const text=$(element).text()
+                  let reso=""
+                  
+                  
+                   if(text.endsWith(".mp4") || text.endsWith(".mkv")){
+                  
+                   if(text.includes("1080p")){
+                    reso="1080p"
+                  }else if(text.includes("720p")){
+                    reso="720p"
+                  }else if(text.includes("480p")){
+                    reso="480p"
+                  }else if(text.includes("360p")){
+                    reso="360p"
+                  }else{
+                    reso=""
+                  }
+                    count++
+                    const size=sizeObj.eq(index-6)?.text() || ""
+                    console.log(count)
+                     fs.appendFileSync(destFile,`{
+                      id:${count},
+                       name:"${obj.name}", 
+                      year:${obj.year},
+                      resolution:"${reso}",
+                      size:"${size}",
+                      href:"${obj.href +  $(element).attr('href')}"
+                       },\n`)
+                   }else{
+                     fs.appendFileSync(extraFile,`{link:"${obj.href+$(element).attr("href")}"},\n`)
+                   }
+               }
+                
+         });
+    
+        
+        
+        
+        
+        console.log("done : ",obj.year)
+     } catch (error) {
+        console.log("err "+error.response);
+    }
+};
+
+
+
+var i=0
+fs.writeFileSync(destFile,"const data = [\n")
+fs.writeFileSync(extraFile,"const data = [\n")
+
+const writeInFile=async ()=>{
+  if(i>=data.length){
+    fs.appendFileSync(destFile ,"]\n module.exports={data}")
+    fs.appendFileSync(extraFile ,"]\n module.exports={data}")
+    return
+  }
+  await extractLinks(data[i])
+  i++
+  writeInFile()
+}
+
+writeInFile()
+
+/*
+const extractLinks = async (url,i) => {
+  //console.log("------loading------")
+    try {
+        const response = await got(url);
+       // console.log("-----loaded-----")
+        const html = response.body;
+        const $ = cheerio.load(html);
+        const linkObjects = $('a');
+        const links = [];
+         linkObjects.each((index, element) => {
+                if(!([0,1,2,3,4,5,6].includes(index))){
+                     fs.appendFileSync(destFile,`{
+                       year:"${$(element).text().replace("/","")}", 
+                      href:"${url +  $(element).attr('href')}"
+                       },\n`)
+               }
+                
+         });
+    
+        
+        
+        
+        
+        console.log("done")
+     } catch (error) {
+        console.log(error.response);
+    }
+};
+const writeInFile=async()=>{
+fs.writeFileSync(destFile,"const data = [\n")
+await extractLinks("http://dl6.sermovie.xyz/Movie/")
+fs.appendFileSync(destFile ,"]\n module.exports={data}")
+}
+
+//---------------------end----------------
+*/
+
+
+
+
+
+/*
+var count=0
+const extractLinks = async (obj) => {
+    try {
+        const response = await got(obj.href);
+        const html = response.body;
+        const $ = cheerio.load(html);
+        const linkObjects = $('a');
+        const links = [];
+         linkObjects.each((index, element) => {
+                if(!([0,1,2,3,4,5,6].includes(index))){
+                    count++
+                    console.log(count)
+                     fs.appendFileSync(destFile,`{
+                      id:${count},
+                       name:"${$(element).text().replaceAll("."," ").replace("/","")}", 
+                      year:${obj.year},
+                      href:"${obj.href +  $(element).attr('href')}"
+                       },\n`)
+               }
+                
+         });
+    
+        
+        
+        
+        
+        console.log("done : ",obj.year)
+     } catch (error) {
+        console.log(error.response);
+    }
+};
+
+
+  
+var i=0
+fs.writeFileSync(destFile,"const data = [\n")
+
+const writeInFile=async ()=>{
+  if(i>=data.length){
+    fs.appendFileSync(destFile ,"]\n module.exports={data}")
+    return
+  }
+  await extractLinks(data[i])
+  i++
+  writeInFile()
+}
+
+writeInFile()
+*/
