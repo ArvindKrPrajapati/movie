55bc8971406ad8bfe2f43ba1f1e2a60ae11725ab
Arvind Kumar <arvind@earnnest.me>
2023-04-27 18:52:21 +0530

    formating

--- 2023/sermovies/movies/years.mjs
+++ 2023/sermovies/movies/years.mjs
@@ -1,109 +1,90 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //find movie link from each folder
-
 
 //const destFile="./data/years.js"
 //const destFile="./data/movie_name.js"
-const destFile="./data/movie_link.js"
-const extraFile="./data/extra.js"
-import * as cheerio from 'cheerio';
-import * as fs from 'fs';
+const destFile = "./data/movie_link.js";
+const extraFile = "./data/extra.js";
+import * as cheerio from "cheerio";
+import * as fs from "fs";
 //import {data} from "./data/years.js"
-import {data} from "./data/movie_name.js"
-const got = (...args) => import('got').then(({ default: got }) => got(...args));
+import { data } from "./data/movie_name.js";
+const got = (...args) => import("got").then(({ default: got }) => got(...args));
 
-var count=0
+var count = 0;
 const extractLinks = async (obj) => {
-    try {
-        const response = await got(obj.href);
-        const html = response.body;
-        const $ = cheerio.load(html);
-        const linkObjects = $('a');
-        const sizeObj=$(".size")
-        
-        const links = [];
-         linkObjects.each((index, element) => {
-                if(!([0,1,2,3,4,5,6].includes(index))){
-                  const text=$(element).text()
-                  let reso=""
-                  
-                  
-                   if(text.endsWith(".mp4") || text.endsWith(".mkv")){
-                  
-                   if(text.includes("1080p")){
-                    reso="1080p"
-                  }else if(text.includes("720p")){
-                    reso="720p"
-                  }else if(text.includes("480p")){
-                    reso="480p"
-                  }else if(text.includes("360p")){
-                    reso="360p"
-                  }else{
-                    reso=""
-                  }
-                    count++
-                    const size=sizeObj.eq(index-6)?.text() || ""
-                    console.log(count)
-                     fs.appendFileSync(destFile,`{
+  try {
+    const response = await got(obj.href);
+    const html = response.body;
+    const $ = cheerio.load(html);
+    const linkObjects = $("a");
+    const sizeObj = $(".size");
+
+    const links = [];
+    linkObjects.each((index, element) => {
+      if (![0, 1, 2, 3, 4, 5, 6].includes(index)) {
+        const text = $(element).text();
+        let reso = "";
+
+        if (text.endsWith(".mp4") || text.endsWith(".mkv")) {
+          if (text.includes("1080p")) {
+            reso = "1080p";
+          } else if (text.includes("720p")) {
+            reso = "720p";
+          } else if (text.includes("480p")) {
+            reso = "480p";
+          } else if (text.includes("360p")) {
+            reso = "360p";
+          } else {
+            reso = "";
+          }
+          count++;
+          const size = sizeObj.eq(index - 6)?.text() || "";
+          console.log(count);
+          fs.appendFileSync(
+            destFile,
+            `{
                       id:${count},
                        name:"${obj.name}", 
                       year:${obj.year},
                       resolution:"${reso}",
                       size:"${size}",
-                      href:"${obj.href +  $(element).attr('href')}"
-                       },\n`)
-                   }else{
-                     fs.appendFileSync(extraFile,`{link:"${obj.href+$(element).attr("href")}"},\n`)
-                   }
-               }
-                
-         });
-    
-        
-        
-        
-        
-        console.log("done : ",obj.year)
-     } catch (error) {
-        console.log("err "+error.response);
-    }
+                      href:"${obj.href + $(element).attr("href")}"
+                       },\n`
+          );
+        } else {
+          fs.appendFileSync(
+            extraFile,
+            `{link:"${obj.href + $(element).attr("href")}"},\n`
+          );
+        }
+      }
+    });
+
+    console.log("done : ", obj.year);
+  } catch (error) {
+    console.log("err " + error.response);
+  }
 };
 
+var i = 0;
+fs.writeFileSync(destFile, "const data = [\n");
+fs.writeFileSync(extraFile, "const data = [\n");
 
-
-var i=0
-fs.writeFileSync(destFile,"const data = [\n")
-fs.writeFileSync(extraFile,"const data = [\n")
-
-const writeInFile=async ()=>{
-  if(i>=data.length){
-    fs.appendFileSync(destFile ,"]\n module.exports={data}")
-    fs.appendFileSync(extraFile ,"]\n module.exports={data}")
-    return
+const writeInFile = async () => {
+  if (i >= data.length) {
+    fs.appendFileSync(destFile, "]\n module.exports={data}");
+    fs.appendFileSync(extraFile, "]\n module.exports={data}");
+    return;
   }
-  await extractLinks(data[i])
-  i++
-  writeInFile()
-}
+  await extractLinks(data[i]);
+  i++;
+  writeInFile();
+};
 
-writeInFile()
+writeInFile();
 
 /*
+-----------------------------start-------------------
 const extractLinks = async (url,i) => {
   //console.log("------loading------")
     try {
@@ -141,11 +122,8 @@ fs.appendFileSync(destFile ,"]\n module.exports={data}")
 //---------------------end----------------
 */
 
-
-
-
-
 /*
+//----------------------------------start-----------------------------
 var count=0
 const extractLinks = async (obj) => {
     try {
@@ -167,10 +145,6 @@ const extractLinks = async (obj) => {
                }
                 
          });
-    
-        
-        
-        
         
         console.log("done : ",obj.year)
      } catch (error) {
@@ -194,4 +168,5 @@ const writeInFile=async ()=>{
 }
 
 writeInFile()
+//-----------------end--------------------------
 */
